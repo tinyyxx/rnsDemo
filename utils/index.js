406b8f8587d747044b9c8edf8f6e7bac573ad728
tinyyxx <726951055@qq.com>
2020-02-11 14:55:57 +0800

    add canReveal and Price

--- utils/index.js
+++ utils/index.js
@@ -1,17 +1,14 @@
-import { randomBytes /* createHash */ } from 'crypto';
-
 const web3 = require('web3');
 const namehash = require('eth-ens-namehash');
 
-
 function numberToUint32(number) {
   const hexDuration = web3.utils.numberToHex(number);
-  let result = '';
-  for (let i = 0; i < 66 - hexDuration.length; i += 1) {
-    result += '0';
+  let _duration = '';
+  for (let i = 0; i < 66 - hexDuration.length; i++) {
+    _duration += '0';
   }
-  result += hexDuration.slice(2);
-  return result;
+  _duration += hexDuration.slice(2);
+  return _duration;
 }
 
 function utf8ToHexString(string) {
@@ -25,42 +22,40 @@ function utf8ToHexString(string) {
  * @param {hex} secret of the commit
  * @param {BN} duration to register in years
  */
-function getRegisterData({
-  name: rawName, owner: rawOwner, secret: rawSecret, duration: rawDuration,
-}) {
+function getRegisterData(name, owner, secret, duration) {
   // 0x + 4 bytes
-  const signature = '0xc2c414c8';
+  const _signature = '0xc2c414c8';
 
   // 20 bytes
-  const owner = rawOwner.toLowerCase().slice(2);
+  const _owner = owner.toLowerCase().slice(2);
 
   // 32 bytes
-  let secret = rawSecret.slice(2);
-  const padding = 64 - secret.length;
-  for (let i = 0; i < padding; i += 1) {
-    secret += '0';
+  let _secret = secret.slice(2);
+  const padding = 64 - _secret.length;
+  for (let i = 0; i < padding; i++) {
+    _secret += '0';
   }
 
   // 32 bytes
-  const duration = numberToUint32(rawDuration);
+  _duration = numberToUint32(duration);
 
   // variable length
-  const name = utf8ToHexString(rawName);
+  const _name = utf8ToHexString(name);
 
-  return `${signature}${owner}${secret}${duration}${name}`;
+  return `${_signature}${_owner}${_secret}${_duration}${_name}`;
 }
 
-function getRenewData(rawName, rawDuration) {
+function getRenewData(name, duration) {
   // 0x + 4 bytes
-  const signature = '0x14b1a4fc';
+  const _signature = '0x14b1a4fc';
 
   // 32 bytes
-  const duration = numberToUint32(rawDuration);
+  _duration = numberToUint32(duration);
 
   // variable length
-  const name = utf8ToHexString(rawName);
+  const _name = utf8ToHexString(name);
 
-  return `${signature}${duration}${name}`;
+  return `${_signature}${_duration}${_name}`;
 }
 
 /**
@@ -70,61 +65,49 @@ function getRenewData(rawName, rawDuration) {
  * @param {hex} secret of the commit
  * @param {BN} duration to register in years
  */
-function getAddrRegisterData({
-  name: rawName, owner: rawOwner, secret: rawSecret, duration: rawDuration, address,
-}) {
+function getAddrRegisterData(name, owner, secret, duration, addr) {
   // 0x + 8 bytes
-  const signature = '0x5f7b99d5';
+  const _signature = '0x5f7b99d5';
 
   // 20 bytes
-  const owner = rawOwner.toLowerCase().slice(2);
+  const _owner = owner.toLowerCase().slice(2);
 
   // 32 bytes
-  let secret = rawSecret.slice(2);
-  const padding = 64 - secret.length;
-  for (let i = 0; i < padding; i += 1) {
-    secret += '0';
+  let _secret = secret.slice(2);
+  const padding = 64 - _secret.length;
+  for (let i = 0; i < padding; i++) {
+    _secret += '0';
   }
 
   // 32 bytes
-  const duration = numberToUint32(rawDuration);
+  _duration = numberToUint32(duration);
 
   // variable length
-  const name = utf8ToHexString(rawName);
+  const _name = utf8ToHexString(name);
 
   // 20 bytes
-  const addr = address.toLowerCase().slice(2);
+  _addr = addr.toLowerCase().slice(2);
 
-  return `${signature}${owner}${secret}${duration}${addr}${name}`;
+  return `${_signature}${_owner}${_secret}${_duration}${_addr}${_name}`;
 }
 
 function getOwner(_rnsInstance, _domainName) {
   return _rnsInstance.methods.owner(namehash.hash(`${_domainName}.rsk`)).call({});
 }
 
-const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+function delay(time) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+}
 
-// Returns a new random alphanumeric string of the given size.
-//
-// Note: to simplify implementation, the result has slight modulo bias,
-// because chars length of 62 doesn't divide the number of all bytes
-// (256) evenly. Such bias is acceptable for most cases when the output
-// length is long enough and doesn't need to be uniform.
-function randomString(size, charRange) {
-  if (size === 0) {
-    throw new Error('Zero-length randomString is useless.');
-  }
-
-  const chars = charRange || ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        + 'abcdefghijklmnopqrstuvwxyz'
-        + '0123456789');
-
-  let objectId = '';
-  const bytes = randomBytes(size);
-  for (let i = 0; i < bytes.length; i += 1) {
-    objectId += chars[bytes.readUInt8(i) % chars.length];
-  }
-  return objectId;
+function getCost(fifsInstance, domain, duration) {
+  return new Promise(((resolve) => {
+    fifsInstance.methods.price(domain, 0, duration).call((err, cost) => {
+      if (err) return resolve(err);
+      return resolve(cost);
+    });
+  }));
 }
 
 module.exports = {
@@ -133,5 +116,6 @@ module.exports = {
   getAddrRegisterData,
   getOwner,
   delay,
-  randomString,
+  getCost
 };
+
