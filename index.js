807b8ebdcfe4b2b278711463d46f0af01e93eb3d
tinyyxx <726951055@qq.com>
2020-05-29 13:13:42 +0800

    enhance address format

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const Web3 = require('web3');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 const rskEndpoint = 'https://public-node.testnet.rsk.co';
-const myWalletAddress = '0x66337f0a594a4A2B219A3e88e16C0bf597Aa6417';
 
 const mnemonic = 'fabric arrest space cost embark tell pear balance title girl photo valley'; // 12 word mnemonic
-const provider = new HDWalletProvider(mnemonic, rskEndpoint);
+const provider = new HDWalletProvider(mnemonic, rskEndpoint, 0, 1, false, "m/44'/37310'/0'/0/0");
 
 // HDWalletProvider is compatible with Web3. Use it at Web3 constructor, just like any other Web3 Provider
 const web3 = new Web3(provider);
 
 const faucetAddress = web3.utils.toChecksumAddress('0x248B320687eBf655f9eE7F62F0388c79fBB7b2F4');
+const myWalletAddress = web3.utils.toChecksumAddress('0x51F85355eFC8559B6325d9cC249080Beb9D2bfd5');
 
 const faucet = new web3.eth.Contract([
   {
